86d2626ee48d52926becacd34c2c7f3da2ebb3d4
Daniel Szekeres <szekeresdani@gmail.com>
2019-02-18 07:23:35 -0800

    Unit test for default speed limit added

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,28 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.mock;
 
 public class TrainSensorTest {
 
     @Before
     public void before() {
-        // TODO Add initializations
+
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
+    }
+
+    @Test
+    public void TestDefaultSpeedLimit() {
+        TrainSensor testSensor = new TrainSensorImpl(mock(TrainController.class), mock(TrainUser.class));
+        assert testSensor.getSpeedLimit() == 4;
     }
 }
